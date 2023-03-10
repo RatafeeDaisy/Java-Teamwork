16fda4d11b3b9c7d302f376095293fb317e70046
s303student <eret@163.com>
2023-03-10 09:08:44 +0800

    添加两个控件

--- src/com/six/helloworld/TaoTao.java
+++ src/com/six/helloworld/TaoTao.java
@@ -1,4 +1,30 @@
 package com.six.helloworld;
 
-public class TaoTao {
+
+import javax.swing.*;
+import java.awt.*;
+public class TaoTao extends JFrame {
+    public TaoTao(String title) throws HeadlessException {
+        super(title);
+        init();
+    }
+    public void init(){
+        //设置窗体的大小
+        setSize(500,500);
+        //设置窗口关闭按钮动作
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //创建画板对象
+        JPanel taotaoJPanel = new JPanel();
+        JLabel myWordsLabel = new JLabel("It is a small world");
+        taotaoJPanel.add(myWordsLabel);
+        //创建输入控件
+        JTextField inputwordsTextField = new JTextField(15);
+        taotaoJPanel.add(inputwordsTextField);
+        JButton showButton = new JButton("show");
+        taotaoJPanel.add(showButton);
+
+        setContentPane(taotaoJPanel);
+        //设置窗口显示
+        setVisible(true);
+    }
 }
