3297b01a63fcde143df0cfd67217b92d4e3603bc
s303student <eret@163.com>
2023-03-10 09:27:49 +0800

    1

--- src/com/six/helloworld/TaoTao.java
+++ src/com/six/helloworld/TaoTao.java
@@ -12,13 +12,14 @@ public class TaoTao extends JFrame {
         //设置窗体的大小
         setSize(500,500);
         //设置窗口关闭按钮动作
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         //创建画板对象
         JPanel taotaoJPanel = new JPanel();
         JLabel myWordsLabel = new JLabel("It is a small world");
         taotaoJPanel.add(myWordsLabel);
         //创建输入控件
         JTextField inputwordsTextField = new JTextField(15);
+        //输入放在画板
         taotaoJPanel.add(inputwordsTextField);
         JButton showButton = new JButton("show");
         taotaoJPanel.add(showButton);
